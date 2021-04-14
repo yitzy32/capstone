c80b61fd59f0aa711abe15f2b0cf62d5aaac0c7f
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-14 16:50:58 -0400

    pantry item create action

---
+++ app/controllers/api/pantry_items_controller.rb
@@ -0,0 +1,11 @@
+class Api::PantryItemsController < ApplicationController
+  def create
+    @pantry_item = PantryItem.new(
+      ingredient_id: params[:ingredient_id],
+      measurement_in_ml: params[:measurement_in_ml],
+      user_id: params[:user_id],
+    )
+    @pantry_item.save
+    render "show.json.jb"
+  end
+end
