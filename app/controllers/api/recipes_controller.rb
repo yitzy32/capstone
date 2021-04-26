ba3e298ff8c57d3d249d097c710b6620481e032a
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-26 19:09:44 -0400

    fix user_id for recipe update seeds file for name column in pantry_items

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,6 +1,6 @@
 class Api::RecipesController < ApplicationController
   def index
-    @recipes = Recipe.where(id: current_user.id)
+    @recipes = Recipe.where(user_id: current_user.id)
     render "index.json.jb"
   end
 
