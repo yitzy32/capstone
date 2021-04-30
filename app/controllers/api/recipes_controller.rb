0c7eb0cc492fdb53f639a8ad2dcb6e5d0b9943e6
Yitzy Bratt <yitzybratt32@gmail.com>
2021-04-30 11:12:04 -0400

    comment out manual recipe create add api url and encrypt key

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -9,52 +9,58 @@ class Api::RecipesController < ApplicationController
     render "show.json.jb"
   end
 
+  # def create
+  #   params[:recipes].each do |recipe|
+  #     @recipe = Recipe.new(
+  #       title: recipe[:title],
+  #       prep_time: recipe[:prep_time],
+  #       servings: recipe[:servings],
+  #       source_url: recipe[:source_url],
+  #       user_id: current_user.id,
+  #     )
+  #     @recipe.save!
+  #   end
+
+  #   params[:directions].each do |direction|
+  #     @direction = Direction.new(
+  #       number: direction[:number],
+  #       step: direction[:step],
+  #       recipe_id: @recipe.id,
+  #     )
+  #     @direction.save!
+  #   end
+
+  #   params[:ingredients].each do |ingredient|
+  #     @ingredient = Ingredient.new(
+  #       name: ingredient[:name],
+  #     )
+  #     @ingredient.save!
+
+  #     @ingredient_recipe = IngredientRecipe.new(
+  #       ingredient_id: @ingredient.id,
+  #       recipe_id: @recipe.id,
+  #       measurement_in_ml: ingredient[:measurement_in_ml],
+  #       number_of: ingredient[:number_of],
+  #     )
+  #     @ingredient_recipe.save!
+  #   end
+
+  #   params[:images].each do |image|
+  #     @image = Image.new(
+  #       url: image[:url],
+  #       alt: image[:alt],
+  #       recipe_id: @recipe.id,
+  #     )
+  #     @image.save!
+  #   end
+
+  #   render "show.json.jb"
+  # end
+
   def create
-    params[:recipes].each do |recipe|
-      @recipe = Recipe.new(
-        title: recipe[:title],
-        prep_time: recipe[:prep_time],
-        servings: recipe[:servings],
-        source_url: recipe[:source_url],
-        user_id: current_user.id,
-      )
-      @recipe.save!
-    end
+    response = HTTP.get("https://api.spoonacular.com/recipes/complexSearch?query=#{params[:search]}&fillIngredients=true&instructionsRequired=true&addRecipeInformation=true&apiKey=#{Rails.application.credentials.spoonacular_api[:api_key]}").to_s
 
-    params[:directions].each do |direction|
-      @direction = Direction.new(
-        number: direction[:number],
-        step: direction[:step],
-        recipe_id: @recipe.id,
-      )
-      @direction.save!
-    end
-
-    params[:ingredients].each do |ingredient|
-      @ingredient = Ingredient.new(
-        name: ingredient[:name],
-      )
-      @ingredient.save!
-
-      @ingredient_recipe = IngredientRecipe.new(
-        ingredient_id: @ingredient.id,
-        recipe_id: @recipe.id,
-        measurement_in_ml: ingredient[:measurement_in_ml],
-        number_of: ingredient[:number_of],
-      )
-      @ingredient_recipe.save!
-    end
-
-    params[:images].each do |image|
-      @image = Image.new(
-        url: image[:url],
-        alt: image[:alt],
-        recipe_id: @recipe.id,
-      )
-      @image.save!
-    end
-
-    render "show.json.jb"
+    render json: response
   end
 
   def update
